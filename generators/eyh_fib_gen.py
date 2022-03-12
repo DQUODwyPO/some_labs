b7ee25748400fb968b5fcf6121874a89482acaee
Dquodwypo <dquodwypo@yandex.ru>
2022-03-13 00:18:32 +0300

    Добавление генератора Фибоначчи

--- generators/eyh_fib_gen.py
+++ generators/eyh_fib_gen.py
@@ -7,6 +7,10 @@ eyh_x0 = 0
 eyh_c = 0
 out_size = 0
 
+fib_r = 0
+fib_s = 0
+
+
 def is_prime(a):
 	cnt = int(sqrt(a))
 	for i in range(2,cnt+1):
@@ -96,6 +100,7 @@ def eyh_read_file():
 	eyh_c = int(l[2])
 	out_size = int(l[3])
 	f.close()
+	print('Прочитаны следующие параметры:\nx0:',eyh_x0,'\na:',eyh_a,'\nc:',eyh_c,'\nN:',out_size)
 	return
 
 
@@ -112,8 +117,67 @@ def start_eyh():
 	eyh_comp()
 	return
 
+def set_fib_par():
+	global fib_r
+	global fib_s
+	global out_size
+	print("Параметр r:", end='')
+	fib_r = input()
+	print("Параметр s:", end='')
+	fib_s = input()
+	print("Длина выходной последовательности:", end='')
+	out_size = input()
+	return
+
+def gen_fib_par():
+	global fib_r
+	global fib_s
+	global out_size
+	out_size = 2**random.randint(5,16)
+	fib_r = random.randint(10,500)
+	fib_s = random.randint(5,100)
+	while fib_s>=fib_r:
+		fib_s = random.randint(5,100)
+	print("Параметр r:",fib_r,"\nПараметр s:",fib_s,"\nДлина выходной последовательности:",out_size)
+	return
+
+def fib_read_file():
+	global fib_r
+	global fib_s
+	global out_size
+	f = open('fib_par.txt','r')
+	l = [line.strip() for line in f]
+	fib_r = int(l[0])
+	fib_s = int(l[1])
+	out_size = int(l[2])
+	f.close()
+	print('Прочитаны следующие параметры:\nr:',fib_r,'\ns:',fib_s,'\nN:',out_size)
+	return
+
+def fib_comp():
+	fibs = []
+	for i in range(fib_r):
+		fibs.append(random.randint(0,out_size))
+	f = open('fib_out.txt','w')
+	for i in range(out_size):
+		x = (fibs[0]+fibs[-fib_s])%out_size
+		f.write(str(x)+"\n")
+		fibs.pop(0)
+		fibs.append(x)
+	f.close()
+	return
 
 def start_fib():
+	print("sPar - Установить параметры вручную")
+	print("gPar - Сгенерировать параметры случайно")
+	cmd = input()
+	if cmd=='sPar':
+		set_fib_par()
+	elif cmd=='gPar':
+		gen_fib_par()
+	else:
+		fib_read_file()
+	fib_comp()
 	return
 
 
